37f15ff99747a38edb56461a2ce0641931a198df
Kranti Joshi <joshikrantee@gmail.com>
2024-09-03 16:58:04 +0545

    Bird game added

--- react/nextjs/src/app/(auth)/birdgame/page.js
+++ react/nextjs/src/app/(auth)/birdgame/page.js
@@ -5,7 +5,10 @@ import React, { useEffect, useState } from 'react'
 const Bird = () => {
     const [randomNum , setRandomNum] = useState(0)
     const [isGameStopped, setIsGameStopped] = useState(false)
-    const birdCages = ['', '💣','']
+    const birdCages = ['', '🎯','']
+    
+    //anonymous function here
+
     useEffect(() => {
         if(!isGameStopped){
             setTimeout(() => {
@@ -14,27 +17,27 @@ const Bird = () => {
                        newNum== 2 ? newNum= 0: newNum=newNum+1
                 }
                  setRandomNum(newNum)
-             }, 50);
+             }, 100);
         }
       }, [randomNum]);
       
       const handleStop = ()=>{
         setIsGameStopped(!isGameStopped)
       }
+      //main logic useEffect ma xa.
   return (
     <div >
        {randomNum}
         {birdCages.map((item, id)=>{
-            return <div className='p-2 m-2 h-44 shadow w-24'>
+            // map wala loop lagako cuz it can return something. 
+            return <div className='p-2 m-2  gap-2 h-52 shadow-md w-52'>
                   {item}
-                {id === randomNum &&  <Image src="https://media.istockphoto.com/id/626132614/photo/blue-fronted-redstart-the-beautiful-blue.jpg?s=612x612&w=0&k=20&c=V9ZpAJrnOPxgf9TiT3wxk77V2TbKqiXl_5yOBL1DP_c="
+                {id === randomNum &&  <Image src="https://upload.wikimedia.org/wikipedia/en/0/02/Tweety.svg"
                     width={100}
                     height={100}
                 />}
-                {id===randomNum && isGameStopped && item &&  <Image src='https://media.tenor.com/aZMV_bT0gVEAAAAi/the-blobs-live-on-bomb.gif'
-                  width={100}
-                  height={100}
-                />
+                {id===randomNum && isGameStopped && item &&  <Image src='https://i.gifer.com/embedded/download/24T.gif'
+                    />
             }
               </div>
         })}
--- react/nextjs/src/redux/store.js
+++ react/nextjs/src/redux/store.js
@@ -2,10 +2,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './reducerSlice/counterSlice'
 import boxSlice from './reducerSlice/boxslice'
+import productSlice from './reducerSlice/productSlice'
+import userSlice from './reducerSlice/userSlice'
+
 
 export default configureStore({
   reducer: {
     counter: counterSlice,
-    box: boxSlice
+    box: boxSlice,
+    product: productSlice,
+    user:userSlice,
   }
 })
