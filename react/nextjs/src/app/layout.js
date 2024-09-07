93de6e2063318ca8c2a51b04ad4f298f2d83f53f
Kranti Joshi <joshikrantee@gmail.com>
2024-09-07 20:12:28 +0545

    counter folder relocated

--- react/nextjs/src/app/layout.js
+++ react/nextjs/src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/redux/reduxProvider";
-import { Children } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
       <ReduxProvider>
         {children}
       </ReduxProvider>
