b875607f00f74e30ddaaadd2fc61cce86e29fd4e
Kranti Joshi <joshikrantee@gmail.com>
2024-09-28 18:51:57 +0545

    calls api for login using postman

--- react/nextjs/src/app/(unauthenticated)/login/page.js
+++ react/nextjs/src/app/(unauthenticated)/login/page.js
@@ -1,27 +1,70 @@
-import { Button, Card, CardBody, DatePicker, Image, Input, Link } from '@nextui-org/react'
-import React from 'react'
+'use client'
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import { Button, Card, CardBody, DatePicker, Image, Input } from '@nextui-org/react';
+import Link from 'next/link';
+import axios from 'axios';
 
-const Login = () => {
-  return (
-    <div className='flex justify-center item-center'>
-    <Card className='flex  w-96 p-4 m-12' >
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .required('Required'),
+    
+});
+
+const Register = () => {
+  //JS ko code haru eta lekhne
+  const handleLogin = async(values)=> {
+  const {data}=await axios.post('https://fakestoreapi.com/auth/login',values )
+  }
+  return(
+  <div>
+
+    <Formik
+      initialValues={{
+        username: '',
+        password: ''
+       
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+       handleLogin(values)
+      }}
+    >
+      {({ errors, touched, handleChange }) => (
+            <div className="flex justify-center items-center">
+            <Card className="flex w-96 p-4 m-12">
+            <CardBody className="flex gap-2">
+        <Form className='flex flex-col gap-4'>
+        <h1 className='text-xl font-mono font-bold text-[#3C5C7D]'>Signup</h1>
         <Image
-      width={300}
-      alt="NextUI hero Image"
-      src="logo.png"
-      /> 
-      <CardBody className='flex gap-2'>
-        <Input placeholder='Enter Email'/>
-        <Input placeholder='Enter Password' type='password'/>
-        <Button className='bg-blue-400 text-white'>Login</Button>
-
-      <p>Don't have an account? <Link href='/register'>Register</Link> Instead.</p>
-      <Link href='#'>Forgot Password</Link>
-      </CardBody>
-
-    </Card>
-    </div>
-  )
+          width={300}
+          alt="NextUI hero Image"
+          src="logo.png"
+        />
+          <Input name="username"    className={errors.username ? ' border border-red-600 rounded-md': null} onChange={handleChange} placeholder="Enter username"/>
+          {errors.username && touched.username ? (
+            <div className='text-red-900 text-sm'>{errors.username}</div>
+          ) : null}
+    
+          <Input name="password" type="password" onChange={handleChange}  placeholder='Enter Password'/>
+          {errors.password && touched.password ? <div>{errors.password}</div> : null}
+          
+          <Button className='bg-[#3C5C7D] text-white' type="submit">Register</Button>
+        </Form>
+       <p> Already have an account? <Link href={"/"}>Sign In</Link> Instead</p>
+        </CardBody>
+        </Card>
+        </div>
+      )}
+    </Formik>
+  </div>
+)
 }
 
-export default Login
+
+export default Register
