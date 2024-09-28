b6ce3ba92210356ee03f9b9d66c3da907e0b9e49
Kranti Joshi <joshikrantee@gmail.com>
2024-09-29 00:06:30 +0545

    adds category items by hitting api

---
+++ react/nextjs/src/app/(auth)/(admin)/layout.js
@@ -0,0 +1,15 @@
+import SideBar from "@/components/sidebar/page";
+import React from "react";
+
+const AdminLayout = ({ children }) => {
+  return (
+    <div>
+        <div className="flex">
+      <SideBar />
+      {children}
+    </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
--- react/nextjs/src/components/sidebar/page.js
+++ react/nextjs/src/components/sidebar/page.js
@@ -1,39 +1,46 @@
 'use client'
+import { Listbox, ListboxItem } from '@nextui-org/react'
 import React from 'react'
-import { FaTrash } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-// import {Listbox, ListboxItem} from "@nextui-org/react";
 
-export default function Sidebar() {
-  const {wishListItems}= useSelector(state=>state.product)
-  const  {name} = useSelector(state=>state.user)
-
+const SideBar = () => {
   return (
-    <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
-
-      {/* <Listbox
-        aria-label="Actions"
-        onAction={(key) => alert(key)}
-      >
-        <ListboxItem key="new">New file</ListboxItem>
-        <ListboxItem key="copy">Copy link</ListboxItem>
-        <ListboxItem key="edit">Edit file</ListboxItem>
-        <ListboxItem key="delete" className="text-danger" color="danger">
-          Delete file
-        </ListboxItem>
-      </Listbox> */}
-
-      {name}, your wishlist item:
-    {wishListItems.length>0 ? wishListItems.map((item, id) => {
-      return <div className='flex'>
-        <li>{id+1}--- {item.title}</li>
-         <FaTrash/>
-        </div>
-    }): "No Wishlist!"}
-
-      </div>
-  );
+    <div>
+      <Listbox className='shadow-md'>
+        <ListboxItem href='/dashboard'>Dashboard</ListboxItem>
+        <ListboxItem href='/categories'>Categories</ListboxItem>
+        <ListboxItem href='/products'>Products</ListboxItem>
+        <ListboxItem href='/users'>Users</ListboxItem>
+      </Listbox>
+    </div>
+  )
 }
+
+
+export default SideBar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -46,30 +53,77 @@ export default function Sidebar() {
 
 
 // 'use client'
-// import React from "react";
-// import { FaTrash } from "react-icons/fa";
-// import { useSelector } from "react-redux";
+// import React from 'react'
+// import { FaTrash } from 'react-icons/fa';
+// import { useSelector } from 'react-redux';
+// // import {Listbox, ListboxItem} from "@nextui-org/react";
 
-
-
-// const Sidebar =() => {
-//     const {wishlistItems}= useSelector(state=>state.product)
-//     const {name} = useSelector(state=>state.user)
+// export default function Sidebar() {
+//   const {wishListItems}= useSelector(state=>state.product)
+//   const  {name} = useSelector(state=>state.user)
 
 //   return (
 //     <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
-    
+
+//       {/* <Listbox
+//         aria-label="Actions"
+//         onAction={(key) => alert(key)}
+//       >
+//         <ListboxItem key="new">New file</ListboxItem>
+//         <ListboxItem key="copy">Copy link</ListboxItem>
+//         <ListboxItem key="edit">Edit file</ListboxItem>
+//         <ListboxItem key="delete" className="text-danger" color="danger">
+//           Delete file
+//         </ListboxItem>
+//       </Listbox> */}
+
 //       {name}, your wishlist item:
-//     {wishlistItems.length>0 ? wishlistItems.map((item,id)=>{
-//         return <div className="flex">
-//             <li>{id+1}-------- {item.productName}</li> 
-//             <FaTrash/>
+//     {wishListItems.length>0 ? wishListItems.map((item, id) => {
+//       return <div className='flex'>
+//         <li>{id+1}--- {item.title}</li>
+//          <FaTrash/>
 //         </div>
-//     }): "No Wishlist!!"}
-//     </div>
+//     }): "No Wishlist!"}
+
+//       </div>
 //   );
 // }
-// export default Sidebar
+
+
+
+
+
+
+
+
+
+
+
+// // 'use client'
+// // import React from "react";
+// // import { FaTrash } from "react-icons/fa";
+// // import { useSelector } from "react-redux";
+
+
+
+// // const Sidebar =() => {
+// //     const {wishlistItems}= useSelector(state=>state.product)
+// //     const {name} = useSelector(state=>state.user)
+
+// //   return (
+// //     <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+    
+// //       {name}, your wishlist item:
+// //     {wishlistItems.length>0 ? wishlistItems.map((item,id)=>{
+// //         return <div className="flex">
+// //             <li>{id+1}-------- {item.productName}</li> 
+// //             <FaTrash/>
+// //         </div>
+// //     }): "No Wishlist!!"}
+// //     </div>
+// //   );
+// // }
+// // export default Sidebar
 
 
 
