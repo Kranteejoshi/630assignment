6d36178974fcb763ba04faa6ddcf9f27393797aa
Kranti Joshi <joshikrantee@gmail.com>
2024-09-29 23:59:43 +0545

    adds category

--- react/nextjs/src/app/(auth)/(admin)/categories/CategoryForm.js
+++ react/nextjs/src/app/(auth)/(admin)/categories/CategoryForm.js
@@ -1,14 +1,91 @@
-'use client'
-import React, { useState } from "react"
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input} from "@nextui-org/react";
+"use client";
+import React, { useState } from "react";
+import {
+  Modal,
+  Button,
+  Card,
+  CardBody,
+  DatePicker,
+  Image,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Input,
+} from "@nextui-org/react";
 
-export default function App() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const [category,setCategory] = useState('')
-  const saveCategory = () =>{
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import {} from "@nextui-org/react";
+import Link from "next/link";
 
-  }
+const FormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  image: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
+const FormInput = () => (
+  <div>
+    <Formik
+      initialValues={{
+        name: "",
+        image: ""
+      }}
+      validationSchema={FormSchema}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched, handleChange }) => (
+        <div className="flex justify-center items-center">
+          <Card className="flex p-4">
+            <CardBody className="flex gap-2">
+              <Form className="flex flex-col">
+                <Input
+                  name="name"
+                  className={
+                    errors.name
+                      ? " border border-red-600 rounded-md"
+                      : null
+                  }
+                  onChange={handleChange}
+                  placeholder="Enter Name"
+                />
+                {errors.name && touched.name ? (
+                  <div className="text-red-900 text-sm">{errors.name}</div>
+                ) : null}
+
+                <Input
+                  name="image"
+                  onChange={handleChange}
+                  placeholder="Enter Image"
+                />
+                {errors.image && touched.image ? (
+                  <div>{errors.image}</div>
+                ) : null}
+                
+                <Button className="bg-[#3C5C7D] text-white" type="submit">
+                  Save
+                </Button>
+              </Form>
+            </CardBody>
+          </Card>
+        </div>
+      )}
+    </Formik>
+  </div>
+);
+
+export default function CategoryForm() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <>
       <Button onPress={onOpen}>Add Category</Button>
@@ -16,11 +93,12 @@ export default function App() {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
               <ModalBody>
-                <Input placeholder="Enter Category Name" value={category} onChange={(e)=>setCategory(e.target.value)}/>
+               <FormInput/>
               </ModalBody>
-              <Button onClick={saveCategory}>Save Category</Button>
             </>
           )}
         </ModalContent>
