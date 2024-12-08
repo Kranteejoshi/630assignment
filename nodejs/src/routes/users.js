789b6e52744d2e50eb4de3c03da7557acf9ebae7
Kranti Joshi <joshikrantee@gmail.com>
2024-12-08 17:10:04 +0545

    creates new schema for user and updates in database

--- nodejs/src/routes/users.js
+++ nodejs/src/routes/users.js
@@ -1,24 +1,53 @@
+
 const {Router} = require('express')
+// const UserRoute = Router()
+// UserRoute.get('/users', (req, res) => {
+//     //fetch users details from database
+//     res.send(['uma', 'krishna', 'hari'])
+//   })
+
+//   UserRoute.post('/users', (req, res) => {
+//     //send users details to database
+//     res.send(['uma', 'krishna', 'hari'])
+//   })
+
+//   UserRoute.put('/users', (req, res) => {
+//     //update users details from database
+//     res.send(['uma', 'krishna', 'hari'])
+//   })
+
+//   UserRoute.delete('/users', (req, res) => {
+//     //delete users details from database
+//     res.send(['uma', 'krishna', 'hari'])
+//   })
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/samajikDb');
+
+const User = mongoose.model('User', { 
+    fullName: String,
+    email: String,
+    gender: {
+      type: String,
+      enum : ['Male', 'Female', 'Other'],
+      default: 'Male'
+    },
+    dateOfBirth: String,
+    password: String,
+    phoneNumber: String
+  });
+
+
 const UserRoute = Router()
-UserRoute.get('/users', (req, res) => {
-    //fetch users details from database
-    res.send(['uma', 'krishna', 'hari'])
-  })
+UserRoute.post('/register', (req, res) => {
+  User.create(req.body)
+  res.send("user created!!")
+})
 
-  UserRoute.post('/users', (req, res) => {
-    //send users details to database
-    res.send(['uma', 'krishna', 'hari'])
-  })
-
-  UserRoute.put('/users', (req, res) => {
-    //update users details from database
-    res.send(['uma', 'krishna', 'hari'])
-  })
-
-  UserRoute.delete('/users', (req, res) => {
-    //delete users details from database
-    res.send(['uma', 'krishna', 'hari'])
-  })
+UserRoute.get('/users', async(req, res) => {
+  const data = await User.find()
+  res.send(data)
+})
 
 
   module.exports = UserRoute;
