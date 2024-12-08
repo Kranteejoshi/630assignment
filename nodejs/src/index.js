66e04f73bc576470135cab2d9e5a611bdd5c9d48
Kranti Joshi <joshikrantee@gmail.com>
2024-12-08 15:30:13 +0545

    mongoose setup for database and nodemon setup

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -6,6 +6,11 @@ const app = express()
 const port = 8000
 app.use(UserRoute)
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/samajikDb');
+
+const User = mongoose.model('User', { name: String, age: Number, isPremiumSubscriber : Boolean });
+User.create({name: "Munu", age: 28, isPremiumSubscriber: true})
 
 
 app.listen(port, () => {
